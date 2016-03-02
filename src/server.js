5b5913cf65bfcf6ee5222d33349f361214f3a116
SJTreadway <treadway08@gmail.com>
2016-03-02 11:44:02 -0600

    gets api key

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const key = require('./secret/key');
 
 mongoose.Promise = require('q').Promise;
 
